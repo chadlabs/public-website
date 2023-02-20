ac92f1d1fdc6cbb724bb4f95949359bb46f10ff1
Chad Hanson <chad@chadlabs.net>
2023-02-20 00:23:20 -0500

    initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,147 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
+    siteTitle: `ChadLabs`,
+    siteTitleAlt: `ChadLabs`,
+    siteHeadline: `ChadLabs`,
+    siteUrl: `https://chadlabs.net`,
+    siteDescription: `Cybersecurity Topics from ChadLabs.`,
+    siteImage: `/banner.jpg`,
+    siteLanguage: `en`,
+    author: `Chad Hanson`,
+  },
+  trailingSlash: `never`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/chadjhanson/`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({
+              query: { site, allPost },
+            }: {
+              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
+            }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ "content:encoded": content }],
+                }
+              }),
+            query: `{
+  allPost(sort: {date: DESC}) {
+    nodes {
+      title
+      date(formatString: "MMMM D, YYYY")
+      excerpt
+      slug
+    }
+  }
+}`,
+            output: `rss.xml`,
+            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+          },
+        ],
+      },
+    },
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
+
+interface IPostTag {
+  name: string
+  slug: string
+}
+
+interface IPost {
+  slug: string
+  title: string
+  defer: boolean
+  date: string
+  excerpt: string
+  contentFilePath: string
+  html: string
+  timeToRead: number
+  wordCount: number
+  tags: Array<IPostTag>
+  banner: any
+  description: string
+  canonicalUrl: string
+}
+
+interface IAllPost {
+  nodes: Array<IPost>
+}
+
+interface ISiteMetadata {
+  siteTitle: string
+  siteTitleAlt: string
+  siteHeadline: string
+  siteUrl: string
+  siteDescription: string
+  siteImage: string
+  author: string
+}
